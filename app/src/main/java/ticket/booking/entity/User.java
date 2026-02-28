ae9f85fd05043b89f0e3701c635e6060a5e18387
Vaibhav Gupta <vaibhavkrgupta369@gmail.com>
2026-02-28 13:40:23 +0530

    created localDB and methods

--- app/src/main/java/ticket/booking/entity/User.java
+++ app/src/main/java/ticket/booking/entity/User.java
@@ -2,7 +2,7 @@ package ticket.booking.entity;
 
 import java.util.List;
 
-public class user {
+public class User {
     private String name;
     private String password;
     private String hashedPassword;
